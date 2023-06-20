a48e4fa462af3d60b7206a181709cd98bfae7557
gabrielmnt <gabriel.mnt15@gmail.com>
2023-06-20 17:10:47 -0300

    Adding names

--- lib/src/pages/welcome_page.dart
+++ lib/src/pages/welcome_page.dart
@@ -9,21 +9,37 @@ class WelcomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Bem-vindo'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            'Bem-vindo',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          Image.asset(
-            'assets/cat-kitten.gif',
-            width: 200,
-            height: 200,
-            fit: BoxFit.cover,
-          ),
-        ],
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Bem-vindo',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Image.asset(
+              'assets/cat-kitten.gif',
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              child: Text(
+                "Gabriel Feques\nRafael Frazão",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
